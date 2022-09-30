e5d1a73ae612c7b468db66f3becb430b47e7211f
byrontung <btung1@ualberta.ca>
2022-09-30 14:25:40 -0600

    added comments to hello.py

--- hello.py
+++ hello.py
@@ -4,19 +4,22 @@ import os
 
 # This was taken from the lab demo.
 
+#Get all environment variables
 # print("Content-Type: text/plain")
 # print()
 # print(os.environ)
 
+#Get all environment variables as a json
 # print("Content-Type: application/json")
 # print()
 # print(json.dumps(dict(os.environ), indent=4))
 
+#Get user browser info
 # print("Content-Type: text/html")
 # print()
 # print(os.environ['HTTP_USER_AGENT'])
 
-
+#Get query string
 # print("Content-Type: text/html")
 # print()
 # print(f"<p>QUERY_STRING={os.environ['QUERY_STRING']}</p>")
